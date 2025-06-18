6f782bd15a130e747ca6ca72e7d958ea5ccc8b79
Bobo <synctax23@gmail.com>
2025-06-19 00:00:46 +0200

    Refactor Tags | Services

--- src/main/handlers/tags.ts
+++ src/main/handlers/tags.ts
@@ -1,69 +1,54 @@
 import { ipcMain } from 'electron'
 import db from '../db/lowdb.js'
 import { IPC_CHANNELS } from '../channels'
+
 import { Tag } from '../db/types'
-
-import { getDates } from '../helpers/getDates.js'
-const { getToday } = getDates()
-
-import { normalizePositionAfterDeletion } from '../helpers/positionNormalizer.js'
+import { useTags } from '../services/useTags'
+const { addTag, editTag, deleteTag } = useTags()
 
 export function registerTagHandlers() {
+  // GET TAGS ====================================================
   ipcMain.handle(IPC_CHANNELS.GET_TAGS, () => db.data.tags)
 
+  // ADD TAG ====================================================
   ipcMain.handle(IPC_CHANNELS.ADD_TAG, (event, addedTag: Tag) => {
     db.read()
-    const nextId = (db.data.tags.at(-1)?.id || 0) + 1
-    const nextPosition = db.data.tags.length
 
-    if (!addedTag.title || addedTag.title.trim() === '')
-      return { success: false, message: 'Title is required' }
+    const result = addTag(addedTag, db.data.tags)
+    if (!result.titleValid) return { success: false, message: 'Title is required' }
 
-    const newTag = {
-      id: nextId,
-      title: addedTag.title,
-      level: 1,
-      exp_current: 0,
-      exp_needed: 10,
-      time_spent: 0,
-      created_at: getToday(),
-      position: nextPosition,
-    }
-    db.data.tags.push(newTag)
-
+    db.data.tags = result.updatedTags
     db.write()
+
     event.sender.send(IPC_CHANNELS.TAGS_UPDATED)
     return { success: true, message: 'Tag added' }
   })
 
+  // EDIT TAG ====================================================
   ipcMain.handle(IPC_CHANNELS.EDIT_TAG, (event, editedTag: Tag) => {
     db.read()
-    const index = db.data.tags.findIndex((tag) => tag.id === editedTag.id)
-    if (index === -1) return { success: false, message: 'Tag not found' }
 
-    if (!editedTag.title || editedTag.title.trim() === '')
-      return { success: false, message: 'Title is required' }
+    const result = editTag(editedTag, db.data.tags)
+    if (!result.tagExists) return { success: false, message: 'Tag not found' }
+    if (!result.titleValid) return { success: false, message: 'Title is required' }
 
-    const tagToUpdate = db.data.tags[index]
-    tagToUpdate.title = editedTag.title
-
+    db.data.tags = result.updatedTags
     db.write()
+
     event.sender.send(IPC_CHANNELS.TAGS_UPDATED)
     return { success: true, message: 'Tag updated' }
   })
 
+  // DELETE TAG ====================================================
   ipcMain.handle(IPC_CHANNELS.DELETE_TAG, (event, id: number) => {
     db.read()
-    const index = db.data.tags.findIndex((tag) => tag.id === id)
-    if (index === -1) return { success: false, message: 'Tag not found' }
 
-    const tags = db.data.tags
-    const tagToDelete = tags[index]
+    const result = deleteTag(id, db.data.tags)
+    if (!result.tagExists) return { success: false, message: 'Tag not found' }
 
-    tags.splice(index, 1)
-    normalizePositionAfterDeletion(tags, tagToDelete.position)
-
+    db.data.tags = result.updatedTags
     db.write()
+
     event.sender.send(IPC_CHANNELS.TAGS_UPDATED)
     return { success: true, message: 'Tag deleted!' }
   })
---
+++ src/main/services/useTags.ts
@@ -0,0 +1,77 @@
+import { nextID } from '../helpers/nextID'
+import { Tag } from '../db/types'
+
+import { normalizePositionAfterDeletion } from '../helpers/positionNormalizer'
+import { useValidations } from '../helpers/useValidations'
+const { validateExistance, validateTitle, validateCost, validateBalance } = useValidations()
+
+import { getDates } from '../helpers/getDates'
+const { getToday } = getDates()
+
+export function useTags() {
+  const addTag = (addedTag: Tag, allTags: Tag[]) => {
+    const titleValid = validateTitle(addedTag.title)
+
+    if (!titleValid) return { titleValid, updatedTags: allTags }
+
+    const nextId = nextID(allTags)
+    const nextPosition = allTags.length
+
+    const newTag = {
+      id: nextId,
+      title: addedTag.title,
+      level: 1,
+      exp_current: 0,
+      exp_needed: 60,
+      time_spent: 0,
+      created_at: getToday(),
+      position: nextPosition,
+    }
+
+    const updatedTags = [...allTags, newTag]
+
+    return { titleValid, updatedTags }
+  }
+
+  const editTag = (editedTag: Tag, allTags: Tag[]) => {
+    const tagExists = validateExistance(editedTag.id, allTags)
+    const titleValid = validateTitle(editedTag.title)
+
+    if (!tagExists || !titleValid) return { tagExists, titleValid, updatedTags: allTags }
+
+    const updatedTags = allTags.map((tag) => {
+      if (tag.id === editedTag.id) {
+        return {
+          ...tag,
+          title: editedTag.title,
+        }
+      }
+      return { ...tag }
+    })
+
+    return { tagExists, titleValid, updatedTags }
+  }
+
+  const deleteTag = (tagId: number, allTags: Tag[]) => {
+    const tagExists = validateExistance(tagId, allTags)
+    if (!tagExists) return { tagExists, updatedTags: allTags }
+
+    // validation returns tag object if it exists
+    const updatedTagsPreNormalizing = allTags.filter((tag) => tag.id !== tagExists.id)
+
+    const updatedTagsNormalized = normalizePositionAfterDeletion(
+      updatedTagsPreNormalizing,
+      tagExists.position,
+    )
+
+    const updatedTags = updatedTagsNormalized
+
+    return { tagExists, updatedTags }
+  }
+
+  return {
+    addTag,
+    editTag,
+    deleteTag,
+  }
+}
